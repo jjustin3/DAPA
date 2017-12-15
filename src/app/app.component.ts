1cca0755d171ac24c7938012de82964442a50a63
Justin Jackson <justin.jackson@factset.com>
2017-12-15 15:00:23 -0500

    Got ng2 charting working.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  datasets = [
+    {
+      label: "Cost Basis",
+      data: [12, 19, 3, 5, 2, 3]
+    }
+  ];
+  labels = ['January', 'February', 'March', 'April', 'May', 'June'];
+  options = {
+    scales: {
+      yAxes: [{
+        ticks: {
+          beginAtZero: true
+        }
+      }]
+    }
+  };
+
   title = 'Digital Asset Portfolio Analyzer';
 }
