d7da05a68d830e2a8939c207a3c9667b9dfa93ae
Justin Jackson <justin.jackson@factset.com>
2017-12-19 15:52:08 -0500

    Created sample data and expanded out tiles.

--- src/app/tile/tile.component.ts
+++ src/app/tile/tile.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+
+import { AllOptions, AllData } from '../sample';
+
 declare let d3: any;
 
 @Component({
@@ -10,74 +13,14 @@ export class TileComponent implements OnInit {
 
   constructor() { }
 
-  options;
-  data;
+  @Input() title: string;
+  @Input() chartType: string;
+  options: any;
+  data: any;
+
   ngOnInit() {
-    this.options = {
-      chart: {
-        type: 'discreteBarChart',
-        height: 450,
-        margin : {
-          top: 20,
-          right: 20,
-          bottom: 50,
-          left: 55
-        },
-        x: function(d){return d.label;},
-        y: function(d){return d.value;},
-        showValues: true,
-        valueFormat: function(d){
-          return '$' + d3.format(',.2f')(d);
-        },
-        duration: 500,
-        xAxis: {
-          axisLabel: 'Month'
-        },
-        yAxis: {
-          axisLabel: 'Amount ($)',
-          axisLabelDistance: -10
-        }
-      }
-    }
-    this.data = [
-      {
-        key: "Cumulative Return",
-        values: [
-          {
-            "label" : "January" ,
-            "value" : 100
-          } ,
-          {
-            "label" : "February" ,
-            "value" : 400
-          } ,
-          {
-            "label" : "March" ,
-            "value" : 300
-          } ,
-          {
-            "label" : "April" ,
-            "value" : 350
-          } ,
-          {
-            "label" : "May" ,
-            "value" : 250
-          } ,
-          {
-            "label" : "June" ,
-            "value" : 600
-          } ,
-          {
-            "label" : "July" ,
-            "value" : 150
-          } ,
-          {
-            "label" : "August" ,
-            "value" : 90
-          }
-        ]
-      }
-    ];
+    this.options = AllOptions[this.chartType];
+    this.data = AllData[this.chartType];
   }
 
 }
